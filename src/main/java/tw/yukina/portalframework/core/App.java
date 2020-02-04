0f246842bce33f312f0ca6927b39b521221f352b
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-02-04 21:30:16 +0800

    Load module jar

--- src/main/java/tw/yukina/portalframework/core/App.java
+++ src/main/java/tw/yukina/portalframework/core/App.java
@@ -6,14 +6,20 @@ package tw.yukina.portalframework.core;
 import tw.yukina.portalframework.api.*;
 import tw.yukina.portalframework.core.util.*;
 
+import com.google.common.reflect.ClassPath;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import java.lang.reflect.Method;
 import java.io.FileInputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 import java.util.List;
+import java.util.ArrayList;
 
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
@@ -27,6 +33,7 @@ public class App {
         Library library = new Library();
         System.out.println(library.someLibraryMethod());
         
+        List<URL> jarUrlList = new ArrayList<>();
         try {
             logger.info("Module Folder Define in " + MODULE_FOLDER_DEFINE_PROPERTIES);
             Path path = FilesUtility.getOrCreateFile(MODULE_FOLDER_DEFINE_PROPERTIES);
@@ -43,13 +50,27 @@ public class App {
             Files.walkFileTree(moduleFolderPath, moduleFileVisitor);
 
             List<Path> jarVisitorPathArrayList = moduleFileVisitor.getPathArrayList();
+            for(Path jarPath : jarVisitorPathArrayList) jarUrlList.add(jarPath.toUri().toURL());
+
+            URLClassLoader jarUrlClassLoader = new URLClassLoader((URL[]) jarUrlList.toArray(new URL[0]));
+            ClassPath classPath = ClassPath.from(jarUrlClassLoader);
+
+            for (ClassPath.ClassInfo classInfo : classPath.getAllClasses()) {
+                if (classInfo.getName().compareTo("tw.yukina.portalframework.core.App") == 0) {
+                    Class mainClass = classInfo.load();
+                    Object mainObject = mainClass.newInstance();
+                    Method startMethod = mainClass.getDeclaredMethod("start",URLClassLoader.class);
+                    startMethod.setAccessible(true);
+                    startMethod.invoke(mainObject,jarUrlClassLoader);
+                }
+            }
 
         } catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    public void start(){
-        
+    public void start(URLClassLoader urlClassLoader){
+        System.out.println("YA");
     }
 }
