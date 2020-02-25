f35bccfd66371be6f846dac80f86572282d72d44
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-02-25 10:15:56 +0800

    Add a service to test initialization

--- src/main/java/tw/yukina/portalframework/core/module/ModuleManager.java
+++ src/main/java/tw/yukina/portalframework/core/module/ModuleManager.java
@@ -1,10 +1,25 @@
 package tw.yukina.portalframework.core.module;
 
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Singleton;
+
+import org.apache.logging.log4j.Logger;
+
 import tw.yukina.portalframework.core.annotation.PreInitialization;
+import tw.yukina.portalframework.core.inject.annotation.InjectLogger;
 import tw.yukina.portalframework.core.service.annotation.Service;
+import tw.yukina.portalframework.core.service.event.*;
 
 @Service
+@Singleton
 @PreInitialization
 public class ModuleManager {
-    
+
+    @InjectLogger
+    private Logger logger;
+
+    @Subscribe
+    public void onPreInit(PreInitializationEvent preInitializationEvent){
+        //logger.info("Module on preInit !!!");
+    }
 }
